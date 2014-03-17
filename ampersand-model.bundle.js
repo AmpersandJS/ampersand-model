5d66291e6c144f264b11b946586234fb88f73e2a
Henrik Joreteg <hjoreteg@gmail.com>
2014-03-17 13:17:34 -0700

    commit bundles

--- ampersand-model.bundle.js
+++ ampersand-model.bundle.js
@@ -3495,7 +3495,74 @@ module.exports = StateyBase;
 }).call(this);
 
 })()
-},{}],7:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
+(function(){var _ = require('underscore');
+var $ = require('jquery-browserify');
+
+
+module.exports = function(method, model, options) {
+    var type = methodMap[method];
+
+    // Default options, unless specified.
+    _.defaults(options || (options = {}), {
+        emulateHTTP: false,
+        emulateJSON: false
+    });
+
+    // Default JSON-request options.
+    var params = {type: type, dataType: 'json'};
+
+    // Ensure that we have a URL.
+    if (!options.url) {
+        params.url = _.result(model, 'url') || urlError();
+    }
+
+    // Ensure that we have the appropriate request data.
+    if (options.data == null && model && (method === 'create' || method === 'update' || method === 'patch')) {
+        params.contentType = 'application/json';
+        params.data = JSON.stringify(options.attrs || model.toJSON(options));
+    }
+
+    // For older servers, emulate JSON by encoding the request into an HTML-form.
+    if (options.emulateJSON) {
+        params.contentType = 'application/x-www-form-urlencoded';
+        params.data = params.data ? {model: params.data} : {};
+    }
+
+    // For older servers, emulate HTTP by mimicking the HTTP method with `_method`
+    // And an `X-HTTP-Method-Override` header.
+    if (options.emulateHTTP && (type === 'PUT' || type === 'DELETE' || type === 'PATCH')) {
+        params.type = 'POST';
+        if (options.emulateJSON) params.data._method = type;
+        var beforeSend = options.beforeSend;
+        options.beforeSend = function(xhr) {
+            xhr.setRequestHeader('X-HTTP-Method-Override', type);
+            if (beforeSend) return beforeSend.apply(this, arguments);
+        };
+    }
+
+    // Don't process data on a non-GET request.
+    if (params.type !== 'GET' && !options.emulateJSON) {
+        params.processData = false;
+    }
+
+    // Make the request, allowing the user to override any Ajax options.
+    var xhr = options.xhr = $.ajax(_.extend(params, options));
+    model.trigger('request', model, xhr, options);
+    return xhr;
+};
+
+// Map from CRUD to HTTP for our default `Backbone.sync` implementation.
+var methodMap = {
+    'create': 'POST',
+    'update': 'PUT',
+    'patch':  'PATCH',
+    'delete': 'DELETE',
+    'read':   'GET'
+};
+
+})()
+},{"jquery-browserify":7,"underscore":3}],7:[function(require,module,exports){
 (function(){// Uses Node, AMD or browser globals to create a module.
 
 // If you want something that will work in other stricter CommonJS environments,
@@ -12830,74 +12897,7 @@ return jQuery;
 })( window ); }));
 
 })()
-},{}],4:[function(require,module,exports){
-(function(){var _ = require('underscore');
-var $ = require('jquery-browserify');
-
-
-module.exports = function(method, model, options) {
-    var type = methodMap[method];
-
-    // Default options, unless specified.
-    _.defaults(options || (options = {}), {
-        emulateHTTP: false,
-        emulateJSON: false
-    });
-
-    // Default JSON-request options.
-    var params = {type: type, dataType: 'json'};
-
-    // Ensure that we have a URL.
-    if (!options.url) {
-        params.url = _.result(model, 'url') || urlError();
-    }
-
-    // Ensure that we have the appropriate request data.
-    if (options.data == null && model && (method === 'create' || method === 'update' || method === 'patch')) {
-        params.contentType = 'application/json';
-        params.data = JSON.stringify(options.attrs || model.toJSON(options));
-    }
-
-    // For older servers, emulate JSON by encoding the request into an HTML-form.
-    if (options.emulateJSON) {
-        params.contentType = 'application/x-www-form-urlencoded';
-        params.data = params.data ? {model: params.data} : {};
-    }
-
-    // For older servers, emulate HTTP by mimicking the HTTP method with `_method`
-    // And an `X-HTTP-Method-Override` header.
-    if (options.emulateHTTP && (type === 'PUT' || type === 'DELETE' || type === 'PATCH')) {
-        params.type = 'POST';
-        if (options.emulateJSON) params.data._method = type;
-        var beforeSend = options.beforeSend;
-        options.beforeSend = function(xhr) {
-            xhr.setRequestHeader('X-HTTP-Method-Override', type);
-            if (beforeSend) return beforeSend.apply(this, arguments);
-        };
-    }
-
-    // Don't process data on a non-GET request.
-    if (params.type !== 'GET' && !options.emulateJSON) {
-        params.processData = false;
-    }
-
-    // Make the request, allowing the user to override any Ajax options.
-    var xhr = options.xhr = $.ajax(_.extend(params, options));
-    model.trigger('request', model, xhr, options);
-    return xhr;
-};
-
-// Map from CRUD to HTTP for our default `Backbone.sync` implementation.
-var methodMap = {
-    'create': 'POST',
-    'update': 'PUT',
-    'patch':  'PATCH',
-    'delete': 'DELETE',
-    'read':   'GET'
-};
-
-})()
-},{"jquery-browserify":7,"underscore":3}],6:[function(require,module,exports){
+},{}],6:[function(require,module,exports){
 module.exports = require('./backbone-events-standalone');
 
 },{"./backbone-events-standalone":8}],8:[function(require,module,exports){
