ff31214cb98bdc65e0ba2c1ea9c339f56bb4c4db
Henrik Joreteg <hjoreteg@gmail.com>
2014-03-17 22:00:37 -0700

    adding bundled

--- ampersand-model.bundle.js
+++ ampersand-model.bundle.js
@@ -6,181 +6,181 @@ var sync = require('ampersand-sync');
 
 
 module.exports = State.extend({
-  save: function (key, val, options) {
-    var attrs, method, xhr, attributes = this.attributes;
+    save: function (key, val, options) {
+        var attrs, method, xhr, attributes = this.attributes;
 
-    // Handle both `"key", value` and `{key: value}` -style arguments.
-    if (key == null || typeof key === 'object') {
-      attrs = key;
-      options = val;
-    } else {
-      (attrs = {})[key] = val;
+        // Handle both `"key", value` and `{key: value}` -style arguments.
+        if (key == null || typeof key === 'object') {
+            attrs = key;
+            options = val;
+        } else {
+            (attrs = {})[key] = val;
+        }
+
+        options = _.extend({validate: true}, options);
+
+        // If we're not waiting and attributes exist, save acts as
+        // `set(attr).save(null, opts)` with validation. Otherwise, check if
+        // the model will be valid when the attributes, if any, are set.
+        if (attrs && !options.wait) {
+            if (!this.set(attrs, options)) return false;
+        } else {
+            if (!this._validate(attrs, options)) return false;
+        }
+
+        // After a successful server-side save, the client is (optionally)
+        // updated with the server-side state.
+        if (options.parse === void 0) options.parse = true;
+        var model = this;
+        var success = options.success;
+        options.success = function (resp) {
+            var serverAttrs = model.parse(resp, options);
+            if (options.wait) serverAttrs = _.extend(attrs || {}, serverAttrs);
+            if (_.isObject(serverAttrs) && !model.set(serverAttrs, options)) {
+                return false;
+            }
+            if (success) success(model, resp, options);
+            model.trigger('sync', model, resp, options);
+        };
+        wrapError(this, options);
+
+        method = this.isNew() ? 'create' : (options.patch ? 'patch' : 'update');
+        if (method === 'patch') options.attrs = attrs;
+        // if we're waiting we haven't actually set our attributes yet so
+        // we need to do make sure we send right data
+        if (options.wait) options.attrs = _.extend(model.serialize(), attrs);
+        xhr = this.sync(method, this, options);
+
+        return xhr;
+    },
+
+    // Fetch the model from the server. If the server's representation of the
+    // model differs from its current attributes, they will be overridden,
+    // triggering a `"change"` event.
+    fetch: function (options) {
+        options = options ? _.clone(options) : {};
+        if (options.parse === void 0) options.parse = true;
+        var model = this;
+        var success = options.success;
+        options.success = function (resp) {
+            if (!model.set(model.parse(resp, options), options)) return false;
+            if (success) success(model, resp, options);
+            model.trigger('sync', model, resp, options);
+        };
+        wrapError(this, options);
+        return this.sync('read', this, options);
+    },
+
+    // Destroy this model on the server if it was already persisted.
+    // Optimistically removes the model from its collection, if it has one.
+    // If `wait: true` is passed, waits for the server to respond before removal.
+    destroy: function (options) {
+        options = options ? _.clone(options) : {};
+        var model = this;
+        var success = options.success;
+
+        var destroy = function () {
+            model.trigger('destroy', model, model.collection, options);
+        };
+
+        options.success = function (resp) {
+            if (options.wait || model.isNew()) destroy();
+            if (success) success(model, resp, options);
+            if (!model.isNew()) model.trigger('sync', model, resp, options);
+        };
+
+        if (this.isNew()) {
+            options.success();
+            return false;
+        }
+        wrapError(this, options);
+
+        var xhr = this.sync('delete', this, options);
+        if (!options.wait) destroy();
+        return xhr;
+    },
+
+    // Proxy `ampersand-sync` by default -- but override this if you need
+    // custom syncing semantics for *this* particular model.
+    sync: function () {
+        return sync.apply(this, arguments);
+    },
+
+    // serialize does nothing by default
+    serialize: function () {
+        return this._getAttributes(false, true);
+    },
+
+    // Remove model from the registry and unbind events
+    remove: function () {
+        if (this.getId() && this.registry) {
+            _.result(this, 'registry').remove(this.type, this.getId(), this._namespace);
+        }
+        this.trigger('remove', this);
+        this.off();
+        return this;
+    },
+
+    // A model is new if it has never been saved to the server, and lacks an id.
+    isNew: function () {
+        return this.getId() == null;
+    },
+
+    // Default URL for the model's representation on the server -- if you're
+    // using Backbone's restful methods, override this to change the endpoint
+    // that will be called.
+    url: function () {
+        var base = _.result(this, 'urlRoot') || _.result(this.collection, 'url') || urlError();
+        if (this.isNew()) return base;
+        return base + (base.charAt(base.length - 1) === '/' ? '' : '/') + encodeURIComponent(this.getId());
+    },
+
+    // get HTML-escaped value of attribute
+    escape: function (attr) {
+        return _.escape(this.get(attr));
+    },
+
+    // convenience methods for manipulating array properties
+    addListVal: function (prop, value, prepend) {
+        var list = _.clone(this[prop]) || [];
+        if (!_(list).contains(value)) {
+            list[prepend ? 'unshift' : 'push'](value);
+            this[prop] = list;
+        }
+        return this;
+    },
+
+    removeListVal: function (prop, value) {
+        var list = _.clone(this[prop]) || [];
+        if (_(list).contains(value)) {
+            this[prop] = _(list).without(value);
+        }
+        return this;
+    },
+
+    hasListVal: function (prop, value) {
+        return _.contains(this[prop] || [], value);
+    },
+
+    // Check if the model is currently in a valid state.
+    isValid: function (options) {
+        return this._validate({}, _.extend(options || {}, { validate: true }));
     }
-
-    options = _.extend({validate: true}, options);
-
-    // If we're not waiting and attributes exist, save acts as
-    // `set(attr).save(null, opts)` with validation. Otherwise, check if
-    // the model will be valid when the attributes, if any, are set.
-    if (attrs && !options.wait) {
-      if (!this.set(attrs, options)) return false;
-    } else {
-      if (!this._validate(attrs, options)) return false;
-    }
-
-    // After a successful server-side save, the client is (optionally)
-    // updated with the server-side state.
-    if (options.parse === void 0) options.parse = true;
-    var model = this;
-    var success = options.success;
-    options.success = function (resp) {
-      var serverAttrs = model.parse(resp, options);
-      if (options.wait) serverAttrs = _.extend(attrs || {}, serverAttrs);
-      if (_.isObject(serverAttrs) && !model.set(serverAttrs, options)) {
-        return false;
-      }
-      if (success) success(model, resp, options);
-      model.trigger('sync', model, resp, options);
-    };
-    wrapError(this, options);
-
-    method = this.isNew() ? 'create' : (options.patch ? 'patch' : 'update');
-    if (method === 'patch') options.attrs = attrs;
-    // if we're waiting we haven't actually set our attributes yet so
-    // we need to do make sure we send right data
-    if (options.wait) options.attrs = _.extend(model.serialize(), attrs);
-    xhr = this.sync(method, this, options);
-
-    return xhr;
-  },
-
-  // Fetch the model from the server. If the server's representation of the
-  // model differs from its current attributes, they will be overridden,
-  // triggering a `"change"` event.
-  fetch: function (options) {
-    options = options ? _.clone(options) : {};
-    if (options.parse === void 0) options.parse = true;
-    var model = this;
-    var success = options.success;
-    options.success = function (resp) {
-      if (!model.set(model.parse(resp, options), options)) return false;
-      if (success) success(model, resp, options);
-      model.trigger('sync', model, resp, options);
-    };
-    wrapError(this, options);
-    return this.sync('read', this, options);
-  },
-
-  // Destroy this model on the server if it was already persisted.
-  // Optimistically removes the model from its collection, if it has one.
-  // If `wait: true` is passed, waits for the server to respond before removal.
-  destroy: function (options) {
-    options = options ? _.clone(options) : {};
-    var model = this;
-    var success = options.success;
-
-    var destroy = function () {
-      model.trigger('destroy', model, model.collection, options);
-    };
-
-    options.success = function (resp) {
-      if (options.wait || model.isNew()) destroy();
-      if (success) success(model, resp, options);
-      if (!model.isNew()) model.trigger('sync', model, resp, options);
-    };
-
-    if (this.isNew()) {
-      options.success();
-      return false;
-    }
-    wrapError(this, options);
-
-    var xhr = this.sync('delete', this, options);
-    if (!options.wait) destroy();
-    return xhr;
-  },
-
-  // Proxy `ampersand-sync` by default -- but override this if you need
-  // custom syncing semantics for *this* particular model.
-  sync: function () {
-    return sync.apply(this, arguments);
-  },
-
-  // serialize does nothing by default
-  serialize: function () {
-    return this._getAttributes(false, true);
-  },
-
-  // Remove model from the registry and unbind events
-  remove: function () {
-    if (this.getId() && this.registry) {
-      _.result(this, 'registry').remove(this.type, this.getId(), this._namespace);
-    }
-    this.trigger('remove', this);
-    this.off();
-    return this;
-  },
-
-  // A model is new if it has never been saved to the server, and lacks an id.
-  isNew: function () {
-    return this.getId() == null;
-  },
-
-  // Default URL for the model's representation on the server -- if you're
-  // using Backbone's restful methods, override this to change the endpoint
-  // that will be called.
-  url: function () {
-    var base = _.result(this, 'urlRoot') || _.result(this.collection, 'url') || urlError();
-    if (this.isNew()) return base;
-    return base + (base.charAt(base.length - 1) === '/' ? '' : '/') + encodeURIComponent(this.getId());
-  },
-
-  // get HTML-escaped value of attribute
-  escape: function (attr) {
-    return _.escape(this.get(attr));
-  },
-
-  // convenience methods for manipulating array properties
-  addListVal: function (prop, value, prepend) {
-    var list = _.clone(this[prop]) || [];
-    if (!_(list).contains(value)) {
-      list[prepend ? 'unshift' : 'push'](value);
-      this[prop] = list;
-    }
-    return this;
-  },
-
-  removeListVal: function (prop, value) {
-    var list = _.clone(this[prop]) || [];
-    if (_(list).contains(value)) {
-      this[prop] = _(list).without(value);
-    }
-    return this;
-  },
-
-  hasListVal: function (prop, value) {
-    return _.contains(this[prop] || [], value);
-  },
-
-  // Check if the model is currently in a valid state.
-  isValid: function (options) {
-    return this._validate({}, _.extend(options || {}, { validate: true }));
-  }
 });
 
 
 // Throw an error when a URL is needed, and none is supplied.
 var urlError = function () {
-  throw new Error('A "url" property or function must be specified');
+    throw new Error('A "url" property or function must be specified');
 };
 
 // Wrap an optional error callback with a fallback error event.
 var wrapError = function (model, options) {
-  var error = options.error;
-  options.error = function (resp) {
-    if (error) error(model, resp, options);
-    model.trigger('error', model, resp, options);
-  };
+    var error = options.error;
+    options.error = function (resp) {
+        if (error) error(model, resp, options);
+        model.trigger('error', model, resp, options);
+    };
 };
 
 },{"ampersand-state":2,"underscore":3,"ampersand-sync":4}],3:[function(require,module,exports){
@@ -2149,7 +2149,80 @@ Base.extend = extend;
 // Our main exports
 module.exports = Base;
 
-},{"backbone-events-standalone":5,"underscore":3}],6:[function(require,module,exports){
+},{"backbone-events-standalone":5,"underscore":3}],4:[function(require,module,exports){
+(function(){var _ = require('underscore');
+var $ = require('jquery-browserify');
+
+
+// Throw an error when a URL is needed, and none is supplied.
+var urlError = function() {
+    throw new Error('A "url" property or function must be specified');
+};
+
+
+module.exports = function(method, model, options) {
+    var type = methodMap[method];
+
+    // Default options, unless specified.
+    _.defaults(options || (options = {}), {
+        emulateHTTP: false,
+        emulateJSON: false
+    });
+
+    // Default JSON-request options.
+    var params = {type: type, dataType: 'json'};
+
+    // Ensure that we have a URL.
+    if (!options.url) {
+        params.url = _.result(model, 'url') || urlError();
+    }
+
+    // Ensure that we have the appropriate request data.
+    if (options.data == null && model && (method === 'create' || method === 'update' || method === 'patch')) {
+        params.contentType = 'application/json';
+        params.data = JSON.stringify(options.attrs || model.toJSON(options));
+    }
+
+    // For older servers, emulate JSON by encoding the request into an HTML-form.
+    if (options.emulateJSON) {
+        params.contentType = 'application/x-www-form-urlencoded';
+        params.data = params.data ? {model: params.data} : {};
+    }
+
+    // For older servers, emulate HTTP by mimicking the HTTP method with `_method`
+    // And an `X-HTTP-Method-Override` header.
+    if (options.emulateHTTP && (type === 'PUT' || type === 'DELETE' || type === 'PATCH')) {
+        params.type = 'POST';
+        if (options.emulateJSON) params.data._method = type;
+        var beforeSend = options.beforeSend;
+        options.beforeSend = function(xhr) {
+            xhr.setRequestHeader('X-HTTP-Method-Override', type);
+            if (beforeSend) return beforeSend.apply(this, arguments);
+        };
+    }
+
+    // Don't process data on a non-GET request.
+    if (params.type !== 'GET' && !options.emulateJSON) {
+        params.processData = false;
+    }
+
+    // Make the request, allowing the user to override any Ajax options.
+    var xhr = options.xhr = $.ajax(_.extend(params, options));
+    model.trigger('request', model, xhr, options);
+    return xhr;
+};
+
+// Map from CRUD to HTTP for our default `Backbone.sync` implementation.
+var methodMap = {
+    'create': 'POST',
+    'update': 'PUT',
+    'patch':  'PATCH',
+    'delete': 'DELETE',
+    'read':   'GET'
+};
+
+})()
+},{"jquery-browserify":6,"underscore":3}],6:[function(require,module,exports){
 (function(){// Uses Node, AMD or browser globals to create a module.
 
 // If you want something that will work in other stricter CommonJS environments,
@@ -11484,80 +11557,7 @@ return jQuery;
 })( window ); }));
 
 })()
-},{}],4:[function(require,module,exports){
-(function(){var _ = require('underscore');
-var $ = require('jquery-browserify');
-
-
-// Throw an error when a URL is needed, and none is supplied.
-var urlError = function() {
-    throw new Error('A "url" property or function must be specified');
-};
-
-
-module.exports = function(method, model, options) {
-    var type = methodMap[method];
-
-    // Default options, unless specified.
-    _.defaults(options || (options = {}), {
-        emulateHTTP: false,
-        emulateJSON: false
-    });
-
-    // Default JSON-request options.
-    var params = {type: type, dataType: 'json'};
-
-    // Ensure that we have a URL.
-    if (!options.url) {
-        params.url = _.result(model, 'url') || urlError();
-    }
-
-    // Ensure that we have the appropriate request data.
-    if (options.data == null && model && (method === 'create' || method === 'update' || method === 'patch')) {
-        params.contentType = 'application/json';
-        params.data = JSON.stringify(options.attrs || model.toJSON(options));
-    }
-
-    // For older servers, emulate JSON by encoding the request into an HTML-form.
-    if (options.emulateJSON) {
-        params.contentType = 'application/x-www-form-urlencoded';
-        params.data = params.data ? {model: params.data} : {};
-    }
-
-    // For older servers, emulate HTTP by mimicking the HTTP method with `_method`
-    // And an `X-HTTP-Method-Override` header.
-    if (options.emulateHTTP && (type === 'PUT' || type === 'DELETE' || type === 'PATCH')) {
-        params.type = 'POST';
-        if (options.emulateJSON) params.data._method = type;
-        var beforeSend = options.beforeSend;
-        options.beforeSend = function(xhr) {
-            xhr.setRequestHeader('X-HTTP-Method-Override', type);
-            if (beforeSend) return beforeSend.apply(this, arguments);
-        };
-    }
-
-    // Don't process data on a non-GET request.
-    if (params.type !== 'GET' && !options.emulateJSON) {
-        params.processData = false;
-    }
-
-    // Make the request, allowing the user to override any Ajax options.
-    var xhr = options.xhr = $.ajax(_.extend(params, options));
-    model.trigger('request', model, xhr, options);
-    return xhr;
-};
-
-// Map from CRUD to HTTP for our default `Backbone.sync` implementation.
-var methodMap = {
-    'create': 'POST',
-    'update': 'PUT',
-    'patch':  'PATCH',
-    'delete': 'DELETE',
-    'read':   'GET'
-};
-
-})()
-},{"jquery-browserify":6,"underscore":3}],5:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 module.exports = require('./backbone-events-standalone');
 
 },{"./backbone-events-standalone":7}],7:[function(require,module,exports){
